25de50164289066b1a45e0048d1bf300565a5b74
zera272 <masterzera111@gmail.com>
2016-06-03 09:22:10 +0700

    a 2016-06-03 ─ 09:22 ─ Jumat
    On branch master
    Your branch is up-to-date with 'origin/master'.
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   index.php
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- index.php
+++ index.php
@@ -3,8 +3,20 @@
 	$botToken = '220170582:AAHQsnFK7cGL_iFKTb9MlbOr2fSpgvftr7k';
 	$site = "https://api.telegram.org/bot".$botToken;
 
-	$update = file_get_contents($site."/getupdates");
+	// $update = file_get_contents($site."/getUpdates?limit=1");
+	$update = file_get_contents($site."/getUpdates");
+	// $update = file_get_contents("php://input");
 
-	print_r($update);
+	// print_r($update);
+
+	$update_array = json_decode($update, true);
+
+	// print_r($update_array);
+	$message_count = count($update_array['result']);
+	// $results = $update_array['result'][0]['message']['text'];
+	$last_message = $update_array['result'][$message_count - 1]['message']['text'];
+
+
+	echo $last_message;
 
 ?>
