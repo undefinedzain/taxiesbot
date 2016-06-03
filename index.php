c4873bb07e657cec15518d4c4e9bc590579d66f1
zera272 <masterzera111@gmail.com>
2016-06-03 08:45:17 +0700

    a 2016-06-03 ─ 08:45 ─ Jumat
    On branch master
    Your branch is up-to-date with 'origin/master'.
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   index.php
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Taxies Bot
+<?php 
+
+	$botToken = '220170582:AAHQsnFK7cGL_iFKTb9MlbOr2fSpgvftr7k';
+	$site = "https://api.telegram.org/bot".$botToken;
+
+	$update = file_get_contents($site."getupdates");
+
+	print_r($update);
+
+?>
